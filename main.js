9fc27da12a0f45b9270755170c17c081c586f94a
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-07-15 15:21:57 +0900

    「イベントハンドラーと未完成の関数の配置」を追加

--- main.js
+++ main.js
@@ -17,3 +17,22 @@ let insertZ = [
   "melted into a puddle on the sidewalk",
   "turned into a slug and crawled away",
 ];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    let name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300);
+    let temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
